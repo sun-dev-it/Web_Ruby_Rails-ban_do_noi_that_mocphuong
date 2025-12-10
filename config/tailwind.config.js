2189808090e005e579da2af83cc036272edc691e
sun-dev-it <daiphatle123@iphatle123gmail.com>
2025-12-10 19:46:43 +0700

    Add Linux platform for Render

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -4,15 +4,10 @@ module.exports = {
     "./app/views/**/*.html.erb",
     "./app/helpers/**/*.rb",
     "./app/assets/stylesheets/**/*.css",
-    "./app/javascript/**/*.js",
+    "./app/javascript/**/*.js"
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: '#1d4ed8', // ví dụ thêm màu xanh
-        secondary: '#9333ea',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
