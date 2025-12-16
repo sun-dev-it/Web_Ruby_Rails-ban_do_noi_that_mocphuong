abf43dfde5781f79fcec98b977be391fb06fda4d
sun-dev-it <daiphatle123@iphatle123gmail.com>
2025-12-16 17:13:17 +0700

    UI admin

--- app/controllers/admin/requests_controller.rb
+++ app/controllers/admin/requests_controller.rb
@@ -1,6 +1,6 @@
 class Admin::RequestsController < ApplicationController
   before_action :require_admin
-  before_action :require_staffs
+  before_action :require_staff
   before_action :set_request, only: [:show, :destroy]
 
   def index
