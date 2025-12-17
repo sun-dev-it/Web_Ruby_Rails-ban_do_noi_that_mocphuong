30f11e7347e6de33038a38f49cb7ecfd5723a297
sun-dev-it <daiphatle123@iphatle123gmail.com>
2025-12-17 21:21:27 +0700

    fix params product

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -70,6 +70,6 @@ class Admin::ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:name, :price, :description, :material, :category_id, images: [])
+    params.require(:product).permit(:name, :price, :description, :material, :category_id, :promotion, :new_price, images: [])
   end
 end
