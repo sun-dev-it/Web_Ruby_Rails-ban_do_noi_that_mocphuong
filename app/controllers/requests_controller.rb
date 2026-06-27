14b6ba1dd174379e40fee4d4696568574b179afa
Phat Le <daiphatle123@gmail.com>
2026-06-27 14:55:39 +0700

    fix controller RequestsController

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,16 +1,16 @@
 class RequestsController < ApplicationController
-  # Xử lý submit form
   def create
     @request = Request.new(request_params)
 
     if @request.save
-      render root_path
+      redirect_to root_path, notice: "Gửi yêu cầu thành công"
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   private
 
-  # Strong params: chỉ cho phép các trường được submit
   def request_params
     params.require(:request).permit(:name, :phone, :content)
   end
