adb137f2882fdc1a336bf3caf438767e22efdbc1
Phat Le <daiphatle123@gmail.com>
2026-09-14 16:32:56 +0700

    fix UI produst, project

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,12 @@ class CategoriesController < ApplicationController
 
   def index
     @categories = Category.order(:name)
-    @products = Product.order(created_at: :desc)
+    @products_per_page = 8
+    @page = [params.fetch(:page, 1).to_i, 1].max
+    @total_products = Product.count
+    @products = Product.order(created_at: :desc).limit(@page * @products_per_page)
+    @has_more_products = @products.length < @total_products
+    @next_page = @page + 1
   end
 
   def show
--- app/controllers/project_informations_controller.rb
+++ app/controllers/project_informations_controller.rb
@@ -1,7 +1,13 @@
 class ProjectInformationsController < ApplicationController
   before_action :set_project_information, only: [:show]
   def index
-    @project_informations = ProjectInformation.where(featured: false).order(created_at: :desc)
+    @projects_per_page = 6
+    @page = [params.fetch(:page, 1).to_i, 1].max
+    all_other_projects = ProjectInformation.where(featured: false).order(created_at: :desc)
+    @total_other_projects = all_other_projects.count
+    @project_informations = all_other_projects.limit(@page * @projects_per_page)
+    @has_more_projects = @project_informations.length < @total_other_projects
+    @next_page = @page + 1
     @project_informations_featured = ProjectInformation.where(featured: true).order(created_at: :desc)
     @projectInformationInfor = ProjectInformationInfor.first
   end
