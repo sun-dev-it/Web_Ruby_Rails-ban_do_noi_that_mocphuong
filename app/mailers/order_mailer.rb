a5a7c32b8497e582ae2cf9f7f7295dbdaf43eee1
Phat Le <daiphatle123@gmail.com>
2026-09-24 00:22:21 +0700

    fix send mail

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -3,13 +3,13 @@ class OrderMailer < ApplicationMailer
     @order = order
     @user = order.user
 
-    admin_emails = ENV.fetch("ADMIN_EMAILS", "")
+    admin_get_emails = ENV.fetch("ADMIN_GET_MAILS", "")
                        .split(",")
                        .map(&:strip)
                        .reject(&:blank?)
 
     mail(
-      to: admin_emails,
+      to: admin_get_emails,
       subject: "[MocPhuong] Có đơn hàng mới ##{@order.id}"
     )
   end
