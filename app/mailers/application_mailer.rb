4ed3c0e6a115e2276fb8ad7a7a658a8323c7401b
Phat Le <daiphatle123@gmail.com>
2026-09-24 15:06:52 +0700

    fix mail send

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "Mộc Phương <no-reply@mocphuong.vn>"
+  default from: "onboarding@resend.dev"
   #default from: ENV.fetch("MAILER_FROM_EMAIL") use domain https://resend.com/domains/add
   layout "mailer"
 end
